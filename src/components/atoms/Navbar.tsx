aa357f231ba8dfb0946d4d7611b623acb8f2dd2d
이대규 <eorb0717@naver.com>
2024-02-21 18:58:47 +0900

    [Feat] Navbar 수정

--- src/components/atoms/Navbar.tsx
+++ src/components/atoms/Navbar.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { CSSProperties } from 'styled-components';
 import home from '../../assets/home.svg';
-import search from '../../assets/search.svg';
-import store from '../../assets/storefront.svg';
+import { useNavigate } from 'react-router-dom';
+// import search from '../../assets/search.svg';
+// import store from '../../assets/storefront.svg';
 
 const Navbar: React.FC = () => {
+    const navigate = useNavigate();
+    // 홈 버튼 클릭시 메인 페이지 이동
+    const handleHomeButton = () => {
+        navigate('/mainpage');
+    };
     const NavbarWrapper: CSSProperties = {
         display: 'flex',
         justifyContent: 'space-between',
@@ -22,42 +28,43 @@ const Navbar: React.FC = () => {
         fontSize: '18px',
         padding: '0px',
     };
-    const NavbarSearchButtonStyle: CSSProperties = {
-        marginRight: '60px',
-        boxShadow: '8px 8px 5px rgba(185, 185, 185, 0.25)',
-        width: '48px',
-        height: '48px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '22px',
-        padding: '0px',
-    };
-    const NavbarStoreButtonStyle: CSSProperties = {
-        marginLeft: '60px',
-        marginTop: '-47.5px',
-        boxShadow: '8px 8px 5px rgba(185, 185, 185, 0.25)',
-        width: '48px',
-        height: '48px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '20px',
-        padding: '0px',
-    };
+    // const NavbarSearchButtonStyle: CSSProperties = {
+    //     marginRight: '60px',
+    //     boxShadow: '8px 8px 5px rgba(185, 185, 185, 0.25)',
+    //     width: '48px',
+    //     height: '48px',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: '22px',
+    //     padding: '0px',
+    // };
+    // const NavbarStoreButtonStyle: CSSProperties = {
+    //     marginLeft: '60px',
+    //     marginTop: '-47.5px',
+    //     boxShadow: '8px 8px 5px rgba(185, 185, 185, 0.25)',
+    //     width: '48px',
+    //     height: '48px',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: '20px',
+    //     padding: '0px',
+    // };
     return (
         <nav style={NavbarWrapper}>
-            <button style={NavbarHomeButtonStyle}>
+            <button onClick={handleHomeButton} style={NavbarHomeButtonStyle}>
                 <img src={home} />
             </button>
-            <div>
+            {/* MVP 이후 구현 */}
+            {/*<div>
                 <button style={NavbarSearchButtonStyle}>
                     <img src={search} />
                 </button>
                 <button style={NavbarStoreButtonStyle}>
                     <img src={store} />
                 </button>
-            </div>
+            </div> */}
         </nav>
     );
 };
